3a2bde5dd3a5bec80be8cf89feb9671f725b1719
ilightwas <ilightwas@gmail.com>
2023-08-15 07:52:05 -0300

    Update wallpaper script for wayland

--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 
-wallpaper="$(find $WALLPAPERS -type f | egrep "\.png|jpg|jpeg" | dmenu -i -l 40)"
-[ -n "$wallpaper" ] && feh --bg-fill "$wallpaper"
+xcmd() {
+  feh --bg-fill "$wallpaper"
+}
+wcmd() {
+# hyprctl hyprpaper wallpaper "\"DP-2 $wallpaper\""
+swww img "$wallpaper"
+}
+
+if pgrep -x "Xorg" >/dev/null || pgrep -x "X" >/dev/null; then 
+  menu="dmenu -i -l 40"
+  wallcmd=xcmd
+else
+  menu="rofi -dmenu"
+  wallcmd=wcmd
+fi
+
+wallpaper="$(find "$WALLPAPERS" -type f | grep -E "\.png|jpg|jpeg" | $menu )"
+
+[ -n "$wallpaper" ] && "$wallcmd"
 
